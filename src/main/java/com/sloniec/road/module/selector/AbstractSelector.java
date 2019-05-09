356a445e367d1c12802b41febaee3454aea7c4ac
Jakub Sloniec <jakub.sloniec@gmail.com>
2019-05-09 18:33:57 +0200

    no waypoints filter, disable time filter for gate without timestamp processing

--- src/main/java/com/sloniec/road/module/selector/AbstractSelector.java
+++ src/main/java/com/sloniec/road/module/selector/AbstractSelector.java
@@ -1,17 +1,20 @@
 package com.sloniec.road.module.selector;
 
-import static com.sloniec.road.shared.Context.getFilterSwitchTimeDistance;
-import static com.sloniec.road.shared.Context.getFilterValueTimeDistance;
-import static com.sloniec.road.shared.commons.TimeCommons.seconds;
-
 import com.sloniec.road.framework.interf.ISelector;
+import com.sloniec.road.shared.Context;
 import com.sloniec.road.shared.commons.GpxFileReader;
 import com.sloniec.road.shared.gpxparser.modal.Waypoint;
+import lombok.extern.slf4j.Slf4j;
+import org.xml.sax.SAXParseException;
+
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import lombok.extern.slf4j.Slf4j;
-import org.xml.sax.SAXParseException;
+
+import static com.sloniec.road.framework.config.ProcessingType.PRZEPUST_BEZ_CZASU;
+import static com.sloniec.road.shared.Context.getFilterSwitchTimeDistance;
+import static com.sloniec.road.shared.Context.getFilterValueTimeDistance;
+import static com.sloniec.road.shared.commons.TimeCommons.seconds;
 
 @Slf4j
 public abstract class AbstractSelector implements ISelector {
@@ -32,9 +35,9 @@ public abstract class AbstractSelector implements ISelector {
         log.info("Liczba wszystkich danych: [{}]", files.size());
 
         List<String> selectedFiles = files.stream()
-            .filter(getBaseFilters()
-                .and(getFilters()))
-            .collect(Collectors.toList());
+                .filter(getBaseFilters()
+                        .and(getFilters()))
+                .collect(Collectors.toList());
 
         log.info("Liczba wybranych danych: [{}]", selectedFiles.size());
         return selectedFiles;
@@ -44,7 +47,8 @@ public abstract class AbstractSelector implements ISelector {
 
     private Predicate<String> getBaseFilters() {
         return isFileSuccessfullyParsed()
-            .and(isTotalTimeDistanceNotTooGreat());
+                .and(isTotalTimeDistanceNotTooGreat())
+                .and(fileContainsWaypoints());
     }
 
     private Predicate<String> isFileSuccessfullyParsed() {
@@ -61,7 +65,7 @@ public abstract class AbstractSelector implements ISelector {
 
     private Predicate<String> isTotalTimeDistanceNotTooGreat() {
         return file -> {
-            if (getFilterSwitchTimeDistance()) {
+            if (getFilterSwitchTimeDistance() && !PRZEPUST_BEZ_CZASU.equals(Context.getProcessingType())) { // HACK to avoid filter activation when there is no timestamps
                 List<Waypoint> waypoints = fileReader.getWaypoints(file);
                 Double maxTimeDistance = getFilterValueTimeDistance();
 
@@ -69,6 +73,16 @@ public abstract class AbstractSelector implements ISelector {
                     log.info("Trasa powyzej [{}]s w pliku [{}]", maxTimeDistance, file);
                     return false;
                 }
+            }
+            return true;
+        };
+    }
+
+    private Predicate<String> fileContainsWaypoints() {
+        return file -> {
+            if (fileReader.getWaypoints(file).isEmpty()) {
+                log.error("Plik [{}] nie zawiera zadnych punktow", file);
+                return false;
             }
             return true;
         };
